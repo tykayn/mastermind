ebdff1ac520a39096c0720d638bfaf129422ae95
gitprep <gitprep@example.com>
2015-06-16 22:47:40 +0200

    pions en objets avec id et color

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,7 +2,15 @@
   angular.module("myApp", []).controller("MainCtrl", function($rootScope, $scope) {
     $scope.demo = 'WOHOOO';
     $scope.sequence = [];
-    $scope.sequenceAdverse = ['yellow', 'violet', 'green', 'green', 'red'];
+      $scope.sequenceAdverse = [
+          {
+              id: 0,
+              color: 'yellow'
+          }, {
+              id: 0,
+              color: 'yellow'
+          }
+      ];
     $scope.addSequence = function() {
       var i, j, len, lengthLines, lespions, o, obj, ref;
       console.log('add sequence');
@@ -19,17 +27,29 @@
         id: lengthLines,
         pions: lespions
       };
-      console.log('obj', obj);
-      return $scope.lines.push(obj);
+        console.log('lines', $scope.lines);
+        $scope.lines.push(obj);
+        return console.log('lines après', $scope.lines);
     };
     $scope.populateSequence = function() {
-      return $scope.sequence = ['yellow', 'violet', 'green', 'violet', 'red'];
+        return $scope.sequence = [
+            {
+                id: 0,
+                color: 'yellow'
+            }, {
+                id: 0,
+                color: 'yellow'
+            }
+        ];
     };
     $scope.addColor = function(color) {
       if ($scope.sequence.length > 4) {
         $scope.sequence.splice(1, 1);
       }
-      return $scope.sequence.push(color);
+        return $scope.sequence.push({
+            id: $scope.sequence.length,
+            color: color
+        });
     };
     $scope.deleteColor = function(index) {
       return $scope.sequence.splice(index, 1);
@@ -39,6 +59,7 @@
     /*
     évaluer la séquence
     et donner ses stats de réponse
+     TODO
      */
     $scope.result = function(id) {
       return {
--- src/coffee/main.coffee
+++ src/coffee/main.coffee
@@ -6,9 +6,11 @@ angular.module "myApp", []
 
   # construction d'une séquence à ajouter
   $scope.sequence = []
-  $scope.sequenceAdverse = ['yellow', 'violet', 'green', 'green', 'red']
+  $scope.sequenceAdverse = [{id: 0, color: 'yellow'}, {id: 0, color: 'yellow'}]
+  #  $scope.sequenceAdverse = []
 
   # ajouter à la séquence
+  # TODO débug de cycle de digest
   $scope.addSequence = ()->
     console.log('add sequence')
     lespions = []
@@ -20,17 +22,21 @@ angular.module "myApp", []
     obj =
       id: lengthLines
       pions: lespions
-    console.log('obj', obj)
+    console.log('lines', $scope.lines)
     $scope.lines.push(obj)
+    console.log('lines après', $scope.lines)
+
   # ajouter à la séquence
   $scope.populateSequence = ()->
-    $scope.sequence = ['yellow', 'violet', 'green', 'violet', 'red']
+    $scope.sequence = [{id: 0, color: 'yellow'}, {id: 0, color: 'yellow'}]
+  #    $scope.sequence = []
+
 
   $scope.addColor = (color)->
     # si y'a déjà 5 couleurs, enlever la première
     if($scope.sequence.length > 4)
       $scope.sequence.splice(1, 1)
-    $scope.sequence.push(color)
+    $scope.sequence.push({id: $scope.sequence.length, color: color})
   # enlever à la séquence
   $scope.deleteColor = (index)->
     $scope.sequence.splice(index, 1)
@@ -42,6 +48,7 @@ angular.module "myApp", []
   ###
   évaluer la séquence
   et donner ses stats de réponse
+  TODO
   ###
   $scope.result = (id)->
     pions: ['white', 'white', 'black', 'black']
