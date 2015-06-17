41edadc966a9bcff21131615d34e2071f4d9f4ef
Baptiste Lemoine <baptiste.lemoine@cofelyineo-gdfsuez.com>
2015-06-17 13:50:18 +0200

    maj readme

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,15 +2,15 @@
   angular.module("myApp", []).controller("MainCtrl", function($rootScope, $scope) {
     $scope.demo = 'WOHOOO';
     $scope.sequence = [];
-      $scope.sequenceAdverse = [
-          {
-              id: 0,
-              color: 'yellow'
-          }, {
-              id: 0,
-              color: 'yellow'
-          }
-      ];
+    $scope.sequenceAdverse = [
+      {
+        id   : 0,
+        color: 'yellow'
+      }, {
+        id   : 0,
+        color: 'yellow'
+      }
+    ];
     $scope.addSequence = function() {
       var i, j, len, lengthLines, lespions, o, obj, ref;
       console.log('add sequence');
@@ -27,29 +27,29 @@
         id: lengthLines,
         pions: lespions
       };
-        console.log('lines', $scope.lines);
-        $scope.lines.push(obj);
-        return console.log('lines après', $scope.lines);
+      console.log('lines', $scope.lines);
+      $scope.lines.push(obj);
+      return console.log('lines après', $scope.lines);
     };
     $scope.populateSequence = function() {
-        return $scope.sequence = [
-            {
-                id: 0,
-                color: 'yellow'
-            }, {
-                id: 0,
-                color: 'yellow'
-            }
-        ];
+      return $scope.sequence = [
+        {
+          id   : 0,
+          color: 'yellow'
+        }, {
+          id   : 0,
+          color: 'yellow'
+        }
+      ];
     };
     $scope.addColor = function(color) {
       if ($scope.sequence.length > 4) {
         $scope.sequence.splice(1, 1);
       }
-        return $scope.sequence.push({
-            id: $scope.sequence.length,
-            color: color
-        });
+      return $scope.sequence.push({
+        id   : $scope.sequence.length,
+        color: color
+      });
     };
     $scope.deleteColor = function(index) {
       return $scope.sequence.splice(index, 1);
